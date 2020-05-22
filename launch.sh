c01bc5a1fc6a8e41d61e3b17fc5871379570d164
ellbristow <elliott@thebristows.eu>
2020-05-22 19:48:54 +0000

    Update to forge 31.2.0. Update Structurize

--- launch.sh
+++ launch.sh
@@ -1,7 +1,7 @@
 cd /home/minecraft/minecraft-server
 while :
 do
-	java -Xmx6144M -Xms2048M -jar forge-1.15.2-31.1.74.jar
+	java -Xmx6144M -Xms2048M -jar forge-1.15.2-31.2.0.jar
 	echo "Server will restart in 10 seconds. Hit Ctrl+C to quit..."
 	sleep 10
 done
