60419cd359f41fb4a9ab9a66b32ce2fffaf27d1f
ellbristow <elliott@thebristows.eu>
2020-05-22 19:28:42 +0000

    Updated Minecolonies, Aquaculture, BiomesOPlenty, Mr Cray's Vehicles, Immersive Portals

--- launch.sh
+++ launch.sh
@@ -1,2 +1,7 @@
 cd /home/minecraft/minecraft-server
-java -Xmx6144M -Xms2048M -jar forge-1.15.2-31.1.74.jar
+while :
+do
+	java -Xmx6144M -Xms2048M -jar forge-1.15.2-31.1.74.jar
+	echo "Server will restart in 10 seconds. Hit Ctrl+C to quit..."
+	sleep 10
+done
